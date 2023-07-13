5739dcb344354c9be85d8d88407bddc1490aa53e
gitone912 <akash.101@kvsroranchi.in>
2023-07-13 20:42:43 +0530

    first commit

---
+++ src/data/projects-table-data.js
@@ -0,0 +1,65 @@
+export const projectsTableData = [
+  {
+    img: "https://www.python.org/static/community_logos/python-logo-master-v3-TM.png",
+    name: "Python web development",
+    members: [
+      { img: "/img/team-1.jpeg", name: "python" },
+      { img: "/img/team-2.jpeg", name: "django" },
+      { img: "/img/team-3.jpeg", name: "Html" },
+      { img: "/img/team-4.jpeg", name: "Css" },
+    ],
+    budget: "14 hours",
+    completion: 60,
+  },
+  {
+    img: "/img/logo-atlassian.svg",
+    name: "Frontend Development course",
+    members: [
+      { img: "/img/team-2.jpeg", name: "Html" },
+      { img: "/img/team-4.jpeg", name: "Css" },
+    ],
+    budget: "5 hours",
+    completion: 10,
+  },
+  {
+    img: "/img/logo-slack.svg",
+    name: "Java Full course",
+    members: [
+      { img: "/img/team-3.jpeg", name: "Java" },
+      { img: "/img/team-1.jpeg", name: "InteliJ" },
+    ],
+    budget: " 5 hours",
+    completion: 100,
+  },
+  {
+    img: "/img/logo-spotify.svg",
+    name: "Python one shot course 10hours",
+    members: [
+      { img: "/img/team-4.jpeg", name: "array" },
+      { img: "/img/team-3.jpeg", name: "lists" },
+      { img: "/img/team-2.jpeg", name: "loops" },
+      { img: "/img/team-1.jpeg", name: "Python" },
+    ],
+    budget: "6 hours",
+    completion: 60,
+  },
+  {
+    img: "/img/logo-jira.svg",
+    name: "Backend Development course",
+    members: [{ img: "/img/team-4.jpeg", name: "Alexander Smith" }],
+    budget: "4 hours",
+    completion: 25,
+  },
+  {
+    img: "/img/logo-invision.svg",
+    name: "Data Science course",
+    members: [
+      { img: "/img/team-1.jpeg", name: "Machine learning" },
+      { img: "/img/team-4.jpeg", name: "Deep Learning" },
+    ],
+    budget: "2 hours",
+    completion: 40,
+  },
+];
+
+export default projectsTableData;
