1b3fad230b0a3c26e17c4dba854562bd078b5124
gitone912 <akash.101@kvsroranchi.in>
2023-07-19 23:31:55 +0530

    off

--- frontend/src/pages/coursesNotes/videoList.jsx
+++ frontend/src/pages/coursesNotes/videoList.jsx
@@ -1,68 +1,61 @@
-import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
-import {
-  Typography,
-  Alert,
-  Avatar,
-  Card,
-  CardHeader,
-  CardBody,
-} from "@material-tailwind/react";
-import { InformationCircleIcon } from "@heroicons/react/24/outline";
-import { useGetOnePlaylistQuery } from "@/services/courseServiceApi";
-import { Navbar, Footer } from "@/widgets/layout";
-import {
-  ChartPieIcon,
-  UserIcon,
-  UserPlusIcon,
-  ArrowRightOnRectangleIcon,
-} from "@heroicons/react/24/solid";
-
-export function VideoList() {
-  const navbarRoutes = [
-    {
-      name: "dashboard",
-      path: "/dashboard/home",
-      icon: ChartPieIcon,
-    },
-    {
-      name: "profile",
-      path: "/dashboard/home",
-      icon: UserIcon,
-    },
-  ]
-  const {playlistId }=  useParams();
+import React, { useState  , useEffect} from 'react';
+import './style.css';
+import { useGetOnePlaylistQuery } from '@/services/courseServiceApi';
+import { useParams } from 'react-router-dom';
+const VideoList = () => {
   const [videos, setVideos] = useState([]);
-    const Response = useGetOnePlaylistQuery(playlistId);
-    const res = Response.data;
-    useEffect(() => {
-        if (res) {
-            setVideos(res.all_videos);
-        }
-    }, [res]);
+  const [selectedVideo, setSelectedVideo] = useState(videos[0]);
 
-  
-
+  const handleVideoClick = (video) => {
+    setSelectedVideo(video);
+  };
+  const { playlistId } = useParams();
+  const Response = useGetOnePlaylistQuery(playlistId);
+  const res = Response.data;
+  useEffect(() => {
+    if (res) {
+      setVideos(res.all_videos);
+    }
+  }, [res]);
+  console.log('videos', videos);
+  console.log('selectedVideo', selectedVideo);
   return (
     <>
-    <style>
-      {`
-      `}
-    </style>
-     <div className="relative min-h-screen w-full">
-      <div className="container relative z-40 mx-auto p-4">
-        <Navbar routes={navbarRoutes} />
-      </div>
-    {videos?.map(({ id, title, description, course,video_link }) => (
-     <p>{title} {video_link}</p>
-      ))}
-      <div className="container absolute bottom-8 left-2/4 z-10 mx-auto -translate-x-2/4 text-black">
-        <Footer />
-      </div>
-    </div>
-         
+      <main className="container">
+        <section className="main-video">
+          <iframe
+            title={selectedVideo?.title}
+            width="560"
+            height="315"
+            src={`${selectedVideo?.id}`}
             
-          
+            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          ></iframe>
+          <h3 className="title">{selectedVideo?.title}</h3>
+        </section>
+
+        <section className="video-playlist">
+          <h3 className="title text-black">Title of Video Playlist</h3>
+          <p>10 lessons &nbsp; . &nbsp; 50m 48s</p>
+          <div className="videos">
+            {videos?.map((video, i) => (
+              <div
+                key={video.id}
+                className={`video ${selectedVideo.id === video.id ? 'active' : ''}`}
+                onClick={() => handleVideoClick(video)}
+              >
+                <img src="/images/play.svg" alt="" />
+                <p>{i + 1 > 9 ? i + 1 : '0' + (i + 1)}. </p>
+                <h3 className="title">{video.title}</h3>
+                <p className="time">{video.duration}</p>
+              </div>
+            ))}
+          </div>
+        </section>
+      </main>
     </>
   );
-}
+};
+
+export { VideoList };
