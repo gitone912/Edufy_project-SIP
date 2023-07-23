fe288ccd0a4d455d452594f3b91528a2894e777d
gitone912 <akash.101@kvsroranchi.in>
2023-07-24 01:38:21 +0530

    donr

--- frontend/src/pages/coursesNotes/notes.jsx
+++ frontend/src/pages/coursesNotes/notes.jsx
@@ -9,6 +9,10 @@ import {
 } from "@material-tailwind/react";
 import { InformationCircleIcon } from "@heroicons/react/24/outline";
 import { useListAllnotesQuery } from "@/services/courseServiceApi";
+import { useListAllPlaylistsQuery } from "@/services/courseServiceApi";
+import { useUpdateDashboardMutation } from "@/services/courseServiceApi";
+import { useGetOneDashboardQuery } from "@/services/courseServiceApi";
+import { getId } from "@/services/LocalStorageService";
 
 export function Notes() {
   const Response = useListAllnotesQuery();
@@ -16,6 +20,49 @@ export function Notes() {
 
   const handleClick = (NotesId) => {
     window.location.href = `/notes/${NotesId}`;
+  };
+
+  const id = getId();
+  const Response1 = useGetOneDashboardQuery(id);
+
+  if (Response1.isSuccess) {
+    var courses = Response1.data.courses;
+    var videos = Response1.data.videos;
+    var notes = Response1.data.notes;
+    var playlist = Response1.data.playlists;
+    var all_notes = Response1.data.all_notes;
+    console.log("courses", courses);
+    console.log("videos", videos);
+    console.log("notes", notes);
+    console.log("playlist", playlist);
+    console.log("all_notes", all_notes);
+    // Separate lists for IDs
+    var courseIds = Response1.data.courses.map((course) => course.id);
+   
+    var videoIds = Response1.data.videos.map((video) => video.id);
+
+    var noteIds = Response1.data.notes.map((note) => note.id);
+
+    var playlistIds = Response1.data.playlists.map((playlist) => playlist.id);
+
+    var allNoteIds = Response1.data.all_notes.map((note) => note.id);
+    console.log("courseIds", courseIds);
+  }
+  const [updateDashboard, updatePlaylistInfo] = useUpdateDashboardMutation();
+
+  const handleUpdatePlaylist = (notesId) => {
+    noteIds.push(notesId);
+    updateDashboard(
+      {
+        id: id,
+        courses: courseIds,
+        videos: videoIds,
+        notes: noteIds,
+        playlists: playlistIds,
+        all_notes: allNoteIds,
+      }
+    );
+    window.location.href = `/dashboard/home`;
   };
 
   return (
@@ -63,6 +110,12 @@ export function Notes() {
                     className="px-4 py-2 bg-blue-500 text-white rounded-md mt-4"
                   >
                     View Notes
+                  </button>
+                  <button
+                    onClick={() => handleUpdatePlaylist(id)} // Pass the playlist ID when the button is clicked
+                    className="mt-4 rounded-md bg-green-500 px-4 py-2 text-white"
+                  >
+                    add to dashboard
                   </button>
                 </CardBody>
               </Card>
