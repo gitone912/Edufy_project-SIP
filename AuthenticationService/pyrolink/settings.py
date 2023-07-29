4b46d1582c3727a9eb548988a4c4f56d1f0da864
gitone912 <akash.101@kvsroranchi.in>
2023-07-29 21:33:28 +0530

    e

--- AuthenticationService/pyrolink/settings.py
+++ AuthenticationService/pyrolink/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = "django-insecure-pmcjlr$4+guv@(j7b=031n9v1ikqon94t4h=-r2&l#t7qb#__w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.ngrok-free.app',"127.0.0.1",'localhost','singuli.serveo.net']
+ALLOWED_HOSTS = ['.ngrok-free.app',"127.0.0.1",'localhost','singuli.serveo.net','.vercel.app']
 
 
 # Application definition
